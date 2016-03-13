aeebef88c7e68be26e02193204668be3a1af1a45
phpBitko <y.bitko@bigmir.net>
2016-03-13 22:30:28 +0200

    Зробив

--- view/solution/vitaliy/task1/les1.php
+++ view/solution/vitaliy/task1/les1.php
@@ -1,17 +1,17 @@
 ﻿<?php
-$hostName="localhost";
-$databaseName="lesson1_base";
-$userName="root";
-$pswd="";
-$dbh = mysql_connect($hostName, $userName, $pswd) or die("Не можу з'єднатися з MySQL.");
-mysql_select_db($databaseName) or die("Не можу пбідключитися до бази.");
+//$hostName="localhost";
+//$databaseName="lesson1_base";
+//$userName="root";
+//$pswd="";
+//$dbh = mysql_connect($hostName, $userName, $pswd) or die("Не можу з'єднатися з MySQL.");
+//mysql_select_db($databaseName) or die("Не можу пбідключитися до бази.");
 
 echo "Ім'я - " .$_POST['valName'];
 echo "<br />";
 echo "Пароль - ". $_POST['valPassword'];
 $nameVal = $_POST['valName'];
 $pasVal = $_POST['valPassword'];
-$query="INSERT INTO `users` (name, password) VALUES($nameVal, $pasVal)";
-mysql_query($query); 
-mysql_close($dbh);
+//$query="INSERT INTO `users` (name, password) VALUES($nameVal, $pasVal)";
+//mysql_query($query);
+//mysql_close($dbh);
 ?>
