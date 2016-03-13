9e9b75aeaa79fc5f9a789feada99360223739e7f
andrii <b.myphp@gmail.com>
2016-03-13 23:05:21 +0200

    First commit

--- view/solution/andriy/task1/task1.php
+++ view/solution/andriy/task1/task1.php
@@ -1,4 +1,16 @@
 <div class="content">
-    <h3>Замість цього тексту має бути виконане завдання)</h3>
+    <form action="" method = "post">
+        <div> Введіть логін</br><input type="text" name="login"></div>
+        <div> Введіть пароль</br><input type="password" name="pass"></div>
+        <div><input type="submit" name="sub" value="Тисни"</br></div>
+
+        <?php
+        if(isset($_POST['login'], $_POST['pass'])) {
+            echo '<p>Ви ввели логін: ' . $_POST['login'];
+            '</p>';
+            echo '<p>Ви ввели пароль: ' . $_POST['pass'];
+            '</p';
+        }
+        ?>
 
 </div>
