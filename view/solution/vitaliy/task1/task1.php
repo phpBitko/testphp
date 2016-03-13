8df32c401d3fcf45e3030aac0b4aa244fb83fed9
phpBitko <y.bitko@bigmir.net>
2016-03-13 22:25:00 +0200

    Зробив

--- view/solution/vitaliy/task1/task1.php
+++ view/solution/vitaliy/task1/task1.php
@@ -1,4 +1,16 @@
 <div class="content">
-    <h3>Замість цього тексту має бути виконане завдання)</h3>
+    <img src = "pic.jpg">
+    <h2>Напої кава <q>StarBuzz</q>> </h2>
+    <p> кава Мексики, Болівії - 10 грн.<blockquote>Неймовірно прекрасна кава з ароматом
+        стиглої вишні і підсмаженої кориці, отримана із високогінних порід кави.<p> Спробуйте це зараз</p></blockquote></p>
+    <p> капучіно - 15 грн. </p>
+    <p> чай - 8 грн. </p>
+    <form action="les1.php" name = "lesson1" method = "post" >
+        <p>Введіть ім'я
+            <input type="text" name = "valName"></p>
+        <p>Введіть пароль
+            <input type = "password" name="valPassword"></p>
+        <input type ="submit" name = "send" value = "Замовити">
+    </form>
 
 </div>
